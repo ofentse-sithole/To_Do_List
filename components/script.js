ccee3a59fd8c05fc30259c4d5a6e33c42e2d5c83
Ofentse Sithole <otsithole07@gmail.com>
2025-01-14 14:41:54 +0200

    added cursor animation

--- components/script.js
+++ components/script.js
@@ -63,3 +63,58 @@ function showTask() {
 
 // Display saved tasks on page load
 showTask();
+
+
+// Adding Javascript for cursor
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+// Gradient of dark blue to lighter blue colors
+const colors = [
+  "#001f4d", // Darkest blue
+  "#003366",
+  "#004080",
+  "#0059b3",
+  "#0073e6",
+  "#1a8cff",
+  "#4da6ff",
+  "#80bfff",
+  "#b3d9ff", // Light blue
+  "#cce6ff",
+  "#e6f2ff",
+  "#f0f8ff"  // Lightest blue
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
